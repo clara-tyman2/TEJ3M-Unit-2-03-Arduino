c5523a663ad73c100e77e2e3694b360fd7cf1e8d
clara-tyman2 <clara.tyman@stu.ocsb.ca>
2025-02-25 09:39:15 -0500

    main.c and diagram

---
+++ main.c
@@ -0,0 +1,26 @@
+/*
+  Blink with Breadboard and LED with resistor
+
+  Turns LED on for one second, the off for one second, each time increases each time by 1 more second
+
+  created Feb 2025 by Clara T
+ */
+
+ const int PIN_5 = 5;
+
+ int blinkTime = 1000;  // set variable to 1000
+  
+ void setup()
+ {
+   pinMode(PIN_5, OUTPUT);
+ }
+ 
+ void loop()
+ {
+   digitalWrite(PIN_5, HIGH);
+   delay(blinkTime); // Wait for 1000 millisecond(s)
+   digitalWrite(PIN_5, LOW);
+   delay(blinkTime); // Wait for 1000 millisecond(s)
+ 
+   blinkTime = blinkTime + 1000; // add 1 second to the variable blinkTime
+ }
